5af225455dc1ba2a6cd61baa8c0311e863bf88b6
gunaa78 <gunaagunaa592@gmail.com>
2026-09-02 12:40:38 +0530

    Fix resume upload

--- backend/utils/uploadToCloudinary.js
+++ backend/utils/uploadToCloudinary.js
@@ -1,7 +1,7 @@
 const streamifier = require("streamifier");
 const cloudinary = require("../config/cloudinary");
 
-const uploadToCloudinary = (fileBuffer, originalName) => {
+const uploadToCloudinary = (fileBuffer, originalName, folder) => {
   return new Promise((resolve, reject) => {
 
     const publicId =
@@ -11,21 +11,16 @@ const uploadToCloudinary = (fileBuffer, originalName) => {
 
     const stream = cloudinary.uploader.upload_stream(
       {
-        folder: "hikoo/resumes",
+        folder: folder,
         resource_type: "raw",
         public_id: publicId,
       },
       (error, result) => {
-
         if (error) {
           console.error("Cloudinary Upload Error:", error);
           reject(error);
         } else {
-          console.log(
-            "Cloudinary Upload Success:",
-            result.secure_url
-          );
-
+          console.log("Cloudinary Upload Success:", result.secure_url);
           resolve(result);
         }
       }

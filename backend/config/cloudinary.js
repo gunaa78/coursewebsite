27c6d0ab75f86242664124eb8e8afaf3ed7fd794
gunaa78 <gunaagunaa592@gmail.com>
2026-09-02 11:21:05 +0530

    Fix resume upload

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
@@ -6,5 +8,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
+
+console.log("Cloudinary Config:");
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key exists:", !!process.env.CLOUDINARY_API_KEY);
+console.log("API Secret exists:", !!process.env.CLOUDINARY_API_SECRET);
 
 module.exports = cloudinary;
